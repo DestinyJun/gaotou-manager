f867f40e252b0ac85356d23d734c675973df9cfa
longrui <1062376610@qq.com>
2018-08-21 22:18:17 +0800

    常规修改： 1.修改头部 2.修改area部分

--- src/app/business/area/area.component.ts
+++ src/app/business/area/area.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Car} from '../../common/model/user-model';
 
 @Component({
   selector: 'app-area',
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./area.component.css']
 })
 export class AreaComponent implements OnInit {
+  public cars: Car[];
+  cols: any[];
 
   constructor() { }
 
   ngOnInit() {
   }
+  export class ButtonDemo {
 
+  clicks: number = 0;
+
+  count() {
+    this.clicks++;
+  }
+}
 }
--- src/app/business/area/area.module.ts
+++ src/app/business/area/area.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AreaComponent } from './area.component';
 import {AreaRoutersModule} from './area.routers.module';
+import {TableModule} from 'primeng/table';
+import {ButtonModule} from 'primeng/button';
 
 @NgModule({
   imports: [
     CommonModule,
-    AreaRoutersModule
+    AreaRoutersModule,
+    TableModule,
+    ButtonModule
   ],
   declarations: [AreaComponent]
 })
