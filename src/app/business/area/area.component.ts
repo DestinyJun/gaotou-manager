670a041307e3005585d0ebbdfd9f8471b5acf06b
DestinyJun <429771890@qq.com>
2018-09-14 22:05:54 +0800

    项目初始化： （1）接口暂时放弃

--- src/app/business/area/area.component.ts
+++ src/app/business/area/area.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import {ConfirmationService, SelectItem, TreeNode} from 'primeng/api';
+import { SelectItem, TreeNode } from 'primeng/api';
+import { AreaService } from '../../common/services/area.service';
 
 @Component({
   selector: 'app-area',
@@ -8,40 +9,50 @@ import {ConfirmationService, SelectItem, TreeNode} from 'primeng/api';
 })
 
 export class AreaComponent implements OnInit {
-  public display: boolean = false;
-  public province: SelectItem[];
-  public cities: SelectItem[];
-  public country: SelectItem[];
+ // table显示相关
+  public areaDates: TreeNode[];
+  public cols: any[];
+  public province = [];
+  public cities = [];
+  public country = [];
+  // 添加框选中的值
+  public display = false;
   public selectedCar1: string;
   public selectedCar2: string;
   public selectedCar3: string;
-  public files1: TreeNode[];
-  public newfiles: TreeNode;
 
-  public cols: any[];
-
-  constructor(private confirmationService: ConfirmationService) { }
+  constructor(
+    private areaService: AreaService
+  ) { }
 
   ngOnInit() {
-    this.newfiles =   {
-      'data': {
-        'name': '',
-      },
-      'children': [
-        {
-          'data': {
-            'name': '',
-          },
-          'children': [
-            {
-              'data': {'name': ''},
-            }
-          ]
-        }
-      ]
-    };
+    this.getDate();
+     // 表单县初始化
+    this.country = [
+      {label: '江口', value: '江口'},
+      {label: '玉屏', value: '玉屏'},
+      {label: '松桃', value: '松桃'},
+    ];
+    this.cols = [
+      { field: 'name', header: 'Name' },
+      { field: 'size', header: 'Size' },
+      { field: 'type', header: 'Type' }
+    ];
+  }
+  public getDate(): void {
+    this.areaService.getFixationArea().subscribe(
+      (value) => {
+        value.data.map((j, index) => {
+          console.log(j);
+          this.province.push({label: j.areaName, value: j.areaName});
+          j.admionistrativeAreaList.map((k, kindex) => {
+            this.cities.push({label: k.areaName, value: k.areaName});
+          });
+        });
+      }
+    );
     // 表格内容
-    this.files1 = [
+    this.areaDates = [
       {
         'data': {
           'name': '贵州',
@@ -115,52 +126,25 @@ export class AreaComponent implements OnInit {
         ]
       },
     ];
-     // 表单省初始化
-    this.province = [
-      {label: '重庆', value: '重庆'},
-      {label: '吉林', value: '吉林'},
-      {label: '湖南', value: '湖南'},
-    ];
-     // 表单市初始化
-    this.cities = [
-      {label: '遵义', value: '遵义'},
-      {label: '攀枝花', value: '攀枝花'},
-      {label: '娄底', value: '娄底'},
-    ];
-     // 表单县初始化
-    this.country = [
-      {label: '江口', value: '江口'},
-      {label: '玉屏', value: '玉屏'},
-      {label: '松桃', value: '松桃'},
-    ];
-    this.cols = [
-      { field: 'name', header: 'Name' },
-      { field: 'size', header: 'Size' },
-      { field: 'type', header: 'Type' }
-    ];
-
   }
   // 弹窗显示与隐藏
-  showDialog() {
+  public showDialog(): void {
     this.display = true;
   }
-  addData() {
-    this.files1.push(this.newfiles);
-    console.log(this.files1);
+  public addData(): void {
+    /*this.files1.push(this.newfiles);
+    console.log(this.files1);*/
   }
-  public provinceChange(e) {
-    this.newfiles.data.name = e.value;
-    console.log(this.newfiles);
+  public provinceChange(e): void {
+    console.log(e.value);
+  }
+  public citiesChange(e): void {
+    // console.log(this.newfiles);
 
   }
-  public citiesChange(e) {
-    this.newfiles.children[0].data.name = e.value;
+  public countryChange(e): void {
+   /* this.newfiles.children[0].children[0].data.name = e.value;
     console.log(this.newfiles);
-
-  }
-  public countryChange(e) {
-    this.newfiles.children[0].children[0].data.name = e.value;
-    console.log(this.newfiles);
-
+*/
   }
 }
--- src/app/common/services/area.service.ts
+++ src/app/common/services/area.service.ts
@@ -1,8 +1,16 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
 
 @Injectable()
 export class AreaService {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) { }
+  // 查询所有固定行政区域
+  public getFixationArea(): Observable<any> {
+    return this.http.get('http://192.168.28.78:8080/highway-management/administrativeArea/query2Tree');
+  }
 
 }
